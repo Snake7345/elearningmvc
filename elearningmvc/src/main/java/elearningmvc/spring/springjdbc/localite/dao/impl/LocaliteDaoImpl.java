5cb55761c7f5e52d4f7d8acde95d4ddc21eb008c
Snake7345 <thebasketman7@hotmail.com>
2020-07-07 11:57:19 +0200

    Fin de la section 113 - Section 14

--- elearningmvc/src/main/java/elearningmvc/spring/springjdbc/localite/dao/impl/LocaliteDaoImpl.java
+++ elearningmvc/src/main/java/elearningmvc/spring/springjdbc/localite/dao/impl/LocaliteDaoImpl.java
@@ -1,15 +1,28 @@
 package elearningmvc.spring.springjdbc.localite.dao.impl;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import elearningmvc.spring.springjdbc.localite.dao.LocaliteDao;
 import elearningmvc.spring.springjdbc.localite.dto.Localite;
 
 public class LocaliteDaoImpl implements LocaliteDao 
 {
 
+	private JdbcTemplate jdbcTemplate;
 	@Override
-	public int create(Localite localite) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int create(Localite localite) 
+	{
+		String sql = "insert into localite values (?,?,?)";
+		int result = jdbcTemplate.update(sql, localite.getIdLocalite(),localite.getVille(),localite.getCp());
+		return result;
+	}
+	
+	/*****************************Getter & Setter********************************************/
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
 	}
 	
 }
