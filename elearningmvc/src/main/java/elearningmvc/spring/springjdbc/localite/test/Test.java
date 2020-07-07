3743432ea267a62d28ad337781af971c23edca79
Snake7345 <thebasketman7@hotmail.com>
2020-07-07 12:11:19 +0200

    Fin section 115 - Section 14

--- elearningmvc/src/main/java/elearningmvc/spring/springjdbc/localite/test/Test.java
+++ elearningmvc/src/main/java/elearningmvc/spring/springjdbc/localite/test/Test.java
@@ -2,16 +2,20 @@ package elearningmvc.spring.springjdbc.localite.test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
+import elearningmvc.spring.springjdbc.localite.dao.LocaliteDao;
+import elearningmvc.spring.springjdbc.localite.dto.Localite;
 
 public class Test 
 {
 	public static void main(String[] args) 
 	{
-		ApplicationContext context = new ClassPathXmlApplicationContext("elearningmvc/spring/springjdbc/config.xml");
-		JdbcTemplate jdbcTemplate= (JdbcTemplate) context.getBean("jdbcTemplate");
-		String sql = "insert into localite values (?,?,?)";
-		int result = jdbcTemplate.update(sql, new Integer(1), "Charleroi", "6000");
+		ApplicationContext context = new ClassPathXmlApplicationContext("elearningmvc/spring/springjdbc/localite/test/config.xml");
+		LocaliteDao dao= (LocaliteDao) context.getBean("localiteDao");
+		Localite localite = new Localite();
+		localite.setIdLocalite(2);
+		localite.setVille("Bruxelles");
+		localite.setCp(1000);
+		int result = dao.create(localite);
 		System.out.println("Number of records inserted are:" + result);
 	}
 }
